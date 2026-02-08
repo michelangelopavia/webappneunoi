62e58b73d033a127d6c18c69c9faa59863391dfa
michelangelopavia <michelangelopavia@gmail.com>
2026-02-08 22:48:21 +0100

    Force reload by updating index.js timestamp

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Proxy to server/index.js for cPanel Deployment
 // This file allows the Node.js App to start from the project root.
+// Last updated: 2026-02-08 22:48
 require('./server/index.js');
